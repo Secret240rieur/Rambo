26eb473d7aa9f276a43ba93e896e37e7827638f6
Secret240rieur <jawharbeliber@gmail.com>
2023-03-25 15:07:06 -0400

    verification and login dont work

--- Assets/LoginScript.cs
+++ Assets/LoginScript.cs
@@ -14,17 +14,6 @@ public class LoginScript : MonoBehaviour
     [SerializeField] TMP_Text password;
     [SerializeField] TMP_Text email;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void SignUp()
     {
@@ -34,24 +23,63 @@ public class LoginScript : MonoBehaviour
 
     public IEnumerator SignUpBackend()
     {
-        using (var request = new UnityWebRequest("https://parseapi.back4app.com/users", "POST"))
+        using var request = new UnityWebRequest("https://parseapi.back4app.com/users", "POST");
+        request.SetRequestHeader("X-Parse-Application-Id", Back4app.ApplicationId);
+        request.SetRequestHeader("X-Parse-REST-API-Key", Back4app.RestApiKey);
+        request.SetRequestHeader("X-Parse-Revocable-Session", "1");
+        request.SetRequestHeader("Content-Type", "application/json");
+        var data = new { username = email.text, email = email.text, password = password.text };
+        string json = JsonConvert.SerializeObject(data);
+        request.uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(json));
+        request.downloadHandler = new DownloadHandlerBuffer();
+        yield return request.SendWebRequest();
+        if (request.result != UnityWebRequest.Result.Success)
         {
-            request.SetRequestHeader("X-Parse-Application-Id", Back4app.ApplicationId);
-            request.SetRequestHeader("X-Parse-REST-API-Key", Back4app.RestApiKey);
-            request.SetRequestHeader("X-Parse-Revocable-Session", "1");
-            request.SetRequestHeader("Content-Type", "application/json");
-            var data = new {username=email.text,email=email.text,password=password.text};
-            string json = JsonConvert.SerializeObject(data);
-            request.uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(json));
-            request.downloadHandler = new DownloadHandlerBuffer();
-            yield return request.SendWebRequest();
-            if (request.result != UnityWebRequest.Result.Success)
-            {
-                Debug.LogError(request.error);
-                yield break;
-            }
-            Debug.Log(request.downloadHandler.text);
+           StartCoroutine(LoginBack4app());
         }
+        Debug.Log(request.downloadHandler.text);
+        StartCoroutine(VerifyEmail());
     }
 
+
+    public IEnumerator VerifyEmail()
+    {
+        using var request = new UnityWebRequest("https://parseapi.back4app.com/verificationEmailRequest", "POST");
+        request.SetRequestHeader("X-Parse-Application-Id", Back4app.ApplicationId);
+        request.SetRequestHeader("X-Parse-REST-API-Key", Back4app.RestApiKey);
+        request.SetRequestHeader("X-Parse-Revocable-Session", "1");
+        request.SetRequestHeader("Content-Type", "application/json");
+        var data = new { email = email.text };
+        string json = JsonConvert.SerializeObject(data);
+        request.uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(json));
+        request.downloadHandler = new DownloadHandlerBuffer();
+        yield return request.SendWebRequest();
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError(request.error);
+            yield break;
+        }
+        Debug.Log(request.downloadHandler.text);
+    } 
+    
+    public IEnumerator LoginBack4app()
+    {
+        using var request = new UnityWebRequest("https://parseapi.back4app.com/login", "GET");
+        request.SetRequestHeader("X-Parse-Application-Id", Back4app.ApplicationId);
+        request.SetRequestHeader("X-Parse-REST-API-Key", Back4app.RestApiKey);
+        request.SetRequestHeader("X-Parse-Revocable-Session", "1");
+        request.SetRequestHeader("Content-Type", "application/json");
+        var data = new { username = email.text, password = password.text };
+        string json = JsonConvert.SerializeObject(data);
+        request.uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(json));
+        request.downloadHandler = new DownloadHandlerBuffer();
+        yield return request.SendWebRequest();
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError(request.error);
+            yield break;
+        }
+        Debug.Log(request.downloadHandler.text);
+        gameObject.SetActive(false);
+    }
 }
